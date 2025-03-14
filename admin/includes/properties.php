1040ab4786136441a4201f44184d8252d42527fa
amanstacker <amanstacker@gmail.com>
2025-03-14 18:37:32 +0530

    audio object missing case
    .

--- admin/includes/properties.php
+++ admin/includes/properties.php
@@ -1893,6 +1893,40 @@ function smpg_get_schema_properties( $schema_id, $post_id = null, $tag_id = null
                         ]                      
                     ];                    
                     break;    
+
+                case 'audioobject':
+
+                    $image['label']    = 'Thumbnail Images';
+                    $hours['label']    = 'Duration ( Hours )';
+                    $minutes['label']  = 'Duration ( minutes )';
+                    $seconds['label']  = 'Duration ( seconds )';
+
+                    $properties = [
+                        'is_enable'         => true,
+                        'is_delete_popup'   => false, 
+                        'is_setup_popup'    => false,
+                        'has_warning'       => false,
+                        'id'                => 'audioobject',           
+                        'text'              => 'AudioObject',
+                        'properties'        => [
+                            'name'             => $name,    
+                            'description'      => $description,
+                            'url'              => $url,
+                            'content_url'      => $content_url,
+                            'embed_url'        => $embed_url,
+                            'upload_date'      => $upload_date,
+                            'hours'            => $hours,
+                            'minutes'          => $minutes,
+                            'seconds'          => $seconds,
+                            'in_language'       => $in_language,
+                            'image'            => $image,                            
+                            'author_type'      => $author_type,
+                            'author_name'      => $author_name,                            
+                            'publisher_name'   => $publisher_name,
+                            'publisher_logo'   => $publisher_logo                                                                      
+                        ]                      
+                    ];                    
+                    break;    
                 
                 case 'softwareapplication':
         
